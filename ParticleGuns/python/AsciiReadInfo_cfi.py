324db8d8a59ccea03e6bb6218eb7a2de89b43669
Andrea Marini <andrea.marini@cern.ch>
2015-08-21 17:15:13 +0200

    implementing first version of Ascii reader extra info

---
+++ ParticleGuns/python/AsciiReadInfo_cfi.py
@@ -0,0 +1,7 @@
+
+InfoProducer = cms.EDProducer("AsciiReaderInfo",
+	fVerbosity = cms.untracked.int32(1),
+	fileName = cms.untracked.string("embedding.txt"),
+	)
+
+infoProducerSequence     = cms.Sequence( InfoProducer )
--- ParticleGuns/src/SealModule.cc
+++ ParticleGuns/src/SealModule.cc
@@ -5,3 +5,7 @@
 // define modules
 using gen::AsciiReaderGun;
 DEFINE_FWK_MODULE(AsciiReaderGun);
+
+
+#include "MCProduction/ParticleGuns/interface/AsciiReaderInfo.h"
+DEFINE_FWK_MODULE(AsciiReaderInfo);
