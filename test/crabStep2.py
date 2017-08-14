a0e6385e095333a9c51036a0b915fd5644373a26
Andrea Carlo Marini <andrea.marini@cern.ch>
2017-08-14 12:54:42 +0200

    single gen production LHE-MINIAOD

--- test/crabStep2.py
+++ test/crabStep2.py
@@ -1,29 +1,34 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'PYTHIA8_MC_Higgs_RECO_M-500'
+config.General.requestName = 'DY_Geneva2_DIGIRAW_4MB'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = False
 
 ## JobType
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'step2.py'
+config.JobType.psetName = 'step2_gp.py'
+config.JobType.inputFiles=['pu.py']
+config.JobType.numCores = 4
+config.JobType.maxMemoryMB=4000
 
 ### DATA configuration
-#config.Data.inputDataset = '/HplusToTauNu-M500/amarini-amarini_PrivateMC_HPlusToTauNu_June2015-16aa19d591b8b49c55c4508e7a7c9233/USER'
-config.Data.inputDataset = '/SingleTau_FlatPt_pythia8/amarini-SingleTau_FlatPt_pythia8_RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_GENSIMRAW-5acfbcd257d5e726b08870647d1dc244/USER'
-#config.Data.inputDataset = '/HplusToTauNu/amarini-amarini_PrivateMC_HPlusToTauNu_June2015-9e71add12689a20c13001f387193a79f/USER'
+#config.Data.inputDataset = '/HplusToTauNu_M_200_TuneCUETP8M1_tauola_13TeV_pythia8/amarini-GEN-SIM-71-6edf4b210aa48b81088c0de44a7af6f5/USER'
+#config.Data.inputDataset = '/DYJetsToMuMu_mll105To160_madgraph_MLM_pythia8/amarini-GEN-SIM-be5fa469151fc874ffd8c8a54cc6c28b/USER'
+config.Data.inputDataset = '/DY_mumu_13TEV_PDF4LHCNNLO100_Mll_50_150_as118/amarini-GEN-SIM-9b8153a64748fc8b50e404570305ee0f/USER'
 config.Data.inputDBS = 'phys03'
-config.Data.ignoreLocality = True
+#config.Data.ignoreLocality = True
+config.Data.ignoreLocality = False
 
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 config.Data.totalUnits = -1
-config.Data.outLFNDirBase = '/store/user/%s/mc/' % (getUsernameFromSiteDB())
+#config.Data.outLFNDirBase = '/store/user/%s/mc/' % (getUsernameFromSiteDB())
+#config.Data.outLFNDirBase = '/store/group/phys_higgs/cmshmm/amarini/'
+config.Data.outLFNDirBase = '/store/group/phys_smp/genevamc/v03' 
 config.Data.publication = True
-config.Data.publishDataName ='%s_PrivateMC_HPlusToTauNu_RECO_June2015'% (getUsernameFromSiteDB())
-config.Data.publishDataName ='RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_RECO'
+config.Data.outputDatasetTag ='DIGI-SIM'
 
 config.Site.storageSite = 'T2_CH_CERN'
 
--- test/crabStep3.py
+++ test/crabStep3.py
@@ -1,8 +1,7 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-#config.General.requestName = 'PYTHIA8_MC_Higgs_MINIAOD_M-900'
-config.General.requestName = 'amcAtNLO_MINIAODv2_M200'
+config.General.requestName = 'DY_LO_MLM_AODSIM'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = False
@@ -10,22 +9,22 @@ config.General.transferLogs = False
 ## JobType
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'step3.py'
+config.JobType.numCores = 4
 
 ### DATA configuration
-#config.Data.inputDataset = '/HplusToTauNu-M500/amarini-RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_RECO-ede2f89505a05d00af372143a993465e/USER'
-#config.Data.inputDataset = '/HplusToTauNu/amarini-amarini_PrivateMC_HPlusToTauNu_June2015-9e71add12689a20c13001f387193a79f/USER'
-#config.Data.inputDataset = '/HplusToTauNu-M900/amarini-RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_AODSIM-99de33e1c31a55fb5b45c03bcfa6de96/USER'
-#config.Data.inputDBS = 'phys03'
-config.Data.inputDataset = '/ChargedHiggs_HplusTB_HplusToTauNu_M-200_13TeV_amcatnlo_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/AODSIM'
-config.Data.inputDBS = 'global'
+#config.Data.inputDataset = '/DYJetsToMuMu_mll105To160_madgraph_MLM_pythia8/amarini-DIGI-SIM-16ca0fac1b892ff3c3d45d801745cbbf/USER'
+config.Data.inputDataset = '/DY_mumu_13TEV_PDF4LHCNNLO100_Mll_50_150_as118/amarini-DIGI-SIM-d6bae82ab2e5563af1a5cace28bcd443/USER'
+config.Data.inputDBS = 'phys03'
 config.Data.ignoreLocality = True
 
 config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 5
+config.Data.unitsPerJob = 1
 config.Data.totalUnits = -1
-config.Data.outLFNDirBase = '/store/user/%s/mc/' % (getUsernameFromSiteDB())
+#config.Data.outLFNDirBase = '/store/user/%s/mc/' % (getUsernameFromSiteDB())
+#config.Data.outLFNDirBase = '/store/group/phys_higgs/cmshmm/amarini/'
+config.Data.outLFNDirBase = '/store/group/phys_smp/genevamc/v03' 
 config.Data.publication = True
-config.Data.publishDataName ='RunIISpring15DR74-74X_mcRun2_asymptotic_v2_MINIAODv2'
+config.Data.outputDatasetTag ='AODSIM'
 
 config.Site.storageSite = 'T2_CH_CERN'
 
