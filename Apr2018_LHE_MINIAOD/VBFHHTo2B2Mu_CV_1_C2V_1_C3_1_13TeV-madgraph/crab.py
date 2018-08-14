4e5095938268907d8b720a28651b1bd3da4622f0
Andrea Carlo Marini <andrea.marini@cern.ch>
2018-08-14 11:33:53 +0200

    VBF Powheg as 2016. VBF HH

---
+++ Apr2018_LHE_MINIAOD/VBFHHTo2B2Mu_CV_1_C2V_1_C3_1_13TeV-madgraph/crab.py
@@ -0,0 +1,26 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = 'amarini_Apr2018_VBFHHTo2B2Mu_CV_1_C2V_1_C3_1_13TeV-madgraph'
+config.General.workArea = 'crab_privateMCProduction'
+config.General.transferOutputs = True
+config.General.transferLogs = False
+
+config.JobType.pluginName = 'PrivateMC'
+#config.JobType.disableAutomaticOutputCollection = True
+config.JobType.maxMemoryMB = 2500
+config.JobType.psetName = 'fake.py' ## fake the last step -> step4 + empty source
+config.JobType.inputFiles = ['scriptExe.sh', 'step1.py','step2.py','step3.py','step4.py','pu.py']
+config.JobType.scriptExe='scriptExe.sh'
+config.JobType.numCores=1
+
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = 500
+config.Data.totalUnits = 500000
+config.Data.outLFNDirBase = '/store/group/phys_higgs/cmshmm/%s/' % (getUsernameFromSiteDB())
+config.Data.publication = True
+config.Data.outputPrimaryDataset = 'VBFHHTo2B2Mu_CV_1_C2V_1_C3_1_13TeV-madgraph'
+config.Data.outputDatasetTag ='Fall17_94X_Apr2018_MINIAOD'
+
+config.Site.storageSite = 'T2_CH_CERN'
+
---
+++ Apr2018_LHE_MINIAOD/VBF_MH125_powheg/step1.py
@@ -0,0 +1,198 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: Configuration/GenProduction/python/HIG-RunIIFall17wmLHEGS-00389-fragment.py --fileout file:step1.root --mc --eventcontent RAWSIM,LHE --datatier GEN-SIM,LHE --conditions 93X_mc2017_realistic_v3 --beamspot Realistic25ns13TeVEarly2017Collision --step LHE,GEN,SIM --nThreads 8 --geometry DB:Extended --era Run2_2017 --python_filename step1.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 500
+import FWCore.ParameterSet.Config as cms
+from FWCore.ParameterSet.VarParsing import VarParsing
+options = VarParsing ('analysis')
+options.register('jobNum', 0, VarParsing.multiplicity.singleton,VarParsing.varType.int,"jobNum")
+options.parseArguments()
+
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process('SIM',eras.Run2_2017)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.GeometrySimDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic25ns13TeVEarly2017Collision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(500)
+)
+
+# Input source
+firstLumi=10*options.jobNum+1 ## eventsPerJob/eventsPerLumi*jobNum +1
+process.source = cms.Source("EmptySource",
+		firstLuminosityBlock  = cms.untracked.uint32(firstLumi),
+		numberEventsInLuminosityBlock = cms.untracked.uint32(100)
+        )
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('Configuration/GenProduction/python/HIG-RunIIFall17wmLHEGS-00389-fragment.py nevts:500'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    ),
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(9),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM'),
+        filterName = cms.untracked.string('')
+    ),
+    eventAutoFlushCompressedSize = cms.untracked.int32(20971520),
+    fileName = cms.untracked.string('file:step1.root'),
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+process.LHEoutput = cms.OutputModule("PoolOutputModule",
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('LHE'),
+        filterName = cms.untracked.string('')
+    ),
+    fileName = cms.untracked.string('file:step1_inLHE.root'),
+    outputCommands = process.LHEEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+process.XMLFromDBSource.label = cms.string("Extended")
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '93X_mc2017_realistic_v3', '')
+
+process.generator = cms.EDFilter("Pythia8HadronizerFilter",
+    PythiaParameters = cms.PSet(
+        parameterSets = cms.vstring('pythia8CommonSettings', 
+            'pythia8CP5Settings', 
+            'pythia8PowhegEmissionVetoSettings', 
+            'processParameters'),
+        processParameters = cms.vstring(
+            'POWHEG:nFinal = 3',
+            'SLHA:useDecayTable = off', 
+            '25:m0 = 125.0', 
+            '25:onMode = off', 
+            '25:onIfMatch = 13 -13'),
+        pythia8CP5Settings = cms.vstring('Tune:pp 14', 
+            'Tune:ee 7', 
+            'MultipartonInteractions:ecmPow=0.03344', 
+            'PDF:pSet=20', 
+            'MultipartonInteractions:bProfile=2', 
+            'MultipartonInteractions:pT0Ref=1.41', 
+            'MultipartonInteractions:coreRadius=0.7634', 
+            'MultipartonInteractions:coreFraction=0.63', 
+            'ColourReconnection:range=5.176', 
+            'SigmaTotal:zeroAXB=off', 
+            'SpaceShower:alphaSorder=2', 
+            'SpaceShower:alphaSvalue=0.118', 
+            'SigmaProcess:alphaSvalue=0.118', 
+            'SigmaProcess:alphaSorder=2', 
+            'MultipartonInteractions:alphaSvalue=0.118', 
+            'MultipartonInteractions:alphaSorder=2', 
+            'TimeShower:alphaSorder=2', 
+            'TimeShower:alphaSvalue=0.118'),
+        pythia8CommonSettings = cms.vstring('Tune:preferLHAPDF = 2', 
+            'Main:timesAllowErrors = 10000', 
+            'Check:epTolErr = 0.01', 
+            'Beams:setProductionScalesFromLHEF = off', 
+            'SLHA:keepSM = on', 
+            'SLHA:minMassSM = 1000.', 
+            'ParticleDecays:limitTau0 = on', 
+            'ParticleDecays:tau0Max = 10', 
+            'ParticleDecays:allowPhotonRadiation = on'),
+        pythia8PowhegEmissionVetoSettings = cms.vstring('POWHEG:veto = 1', 
+            'POWHEG:pTdef = 1', 
+            'POWHEG:emitted = 0', 
+            'POWHEG:pTemt = 0', 
+            'POWHEG:pThard = 0', 
+            'POWHEG:vetoCount = 100', 
+            'SpaceShower:pTmaxMatch = 2', 
+            'TimeShower:pTmaxMatch = 2')
+    ),
+    comEnergy = cms.double(13000.0),
+    filterEfficiency = cms.untracked.double(1.0),
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    pythiaPylistVerbosity = cms.untracked.int32(1)
+)
+
+
+process.externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
+    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc481/13TeV/powheg/V2/VBF_H_NNPDF30_13TeV_M125/v1/VBFH_NNPDF30_M-125_13TeV_tarball.tar.gz'),
+    nEvents = cms.untracked.uint32(500),
+    numberOfParameters = cms.uint32(1),
+    outputFile = cms.string('cmsgrid_final.lhe'),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
+)
+
+
+process.ProductionFilterSequence = cms.Sequence(process.generator)
+
+# Path and EndPath definitions
+process.lhe_step = cms.Path(process.externalLHEProducer)
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+process.LHEoutput_step = cms.EndPath(process.LHEoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.lhe_step,process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.RAWSIMoutput_step,process.LHEoutput_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+#Setup FWK for multithreaded
+process.options.numberOfThreads=cms.untracked.uint32(1)
+process.options.numberOfStreams=cms.untracked.uint32(0)
+# filter all path with the production filter sequence
+for path in process.paths:
+	if path in ['lhe_step']: continue
+	getattr(process,path)._seq = process.ProductionFilterSequence * getattr(process,path)._seq 
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.Utils
+from Configuration.DataProcessing.Utils import addMonitoring 
+
+#call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
+process = addMonitoring(process)
+
+# End of customisation functions
+
+# Customisation from command line
+import os,random
+random.seed = os.urandom(10) #~10^14
+process.RandomNumberGeneratorService.externalLHEProducer.initialSeed = random.randint(0,999999)
+process.RandomNumberGeneratorService.generator.initialSeed = random.randint(0,999999)
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
