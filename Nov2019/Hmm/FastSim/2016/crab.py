91e2245f5d9de5a0860349641e752eb1a9801060
Andrea Carlo Marini <andrea.marini@cern.ch>
2019-11-11 18:39:54 +0100

    update

--- Nov2019/Hmm/FastSim/2016/crab.py
+++ Nov2019/Hmm/FastSim/2016/crab.py
@@ -12,15 +12,17 @@ config.JobType.maxMemoryMB = 2500
 config.JobType.psetName = 'fake.py' ## fake the last step -> step4 + empty source
 config.JobType.inputFiles = ['scriptExe.sh', 'step1.py','step2.py','step3.py','pu.py']
 config.JobType.scriptExe='scriptExe.sh'
-config.JobType.numCores=1
+config.JobType.numCores=8
 
 config.Data.splitting = 'EventBased'
 config.Data.unitsPerJob = 500
-config.Data.totalUnits = 2000000
-config.Data.outLFNDirBase = '/store/group/phys_higgs/cmshmm/%s/' % (getUsernameFromSiteDB())
+config.Data.totalUnits = 10000
+#config.Data.outLFNDirBase = '/store/group/phys_higgs/cmshmm/%s/' % (getUsernameFromSiteDB())
+config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 config.Data.publication = True
 config.Data.outputPrimaryDataset = 'GluGlu_HToMuMu_M125_13TeV_powheg_pythia8'
 config.Data.outputDatasetTag ='FastSim_94X-MINIAODSIM'
 
-config.Site.storageSite = 'T2_CH_CERN'
+#config.Site.storageSite = 'T2_CH_CERN'
+config.Site.storageSite = 'T3_US_MIT'
 
