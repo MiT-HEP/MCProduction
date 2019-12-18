a7561f1d371568c2990458e274b23485f3cf406b
Andrea Carlo Marini <andrea.marini@cern.ch>
2019-12-18 16:10:23 +0100

    first try UL

---
+++ UL/Hmm/2017/GluGlu/crab.py
@@ -0,0 +1,28 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = 'amarini_UL2017_GGHMH125_powheg'
+config.General.workArea = 'crab_privateMCProduction'
+config.General.transferOutputs = True
+config.General.transferLogs = False
+
+config.JobType.pluginName = 'PrivateMC'
+#config.JobType.disableAutomaticOutputCollection = True
+config.JobType.maxMemoryMB = 2500
+config.JobType.psetName = 'fake.py' ## fake the last step -> step4 + empty source
+config.JobType.inputFiles = ['scriptExe.sh', 'step1_cfg.py','step2_cfg.py','step3_cfg.py','step4_cfg.py','step5_cfg.py','step6_cfg.py','pu.py']
+config.JobType.scriptExe='scriptExe.sh'
+config.JobType.numCores=8
+
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = 500
+config.Data.totalUnits = 10000
+#config.Data.outLFNDirBase = '/store/group/phys_higgs/cmshmm/%s/' % (getUsernameFromSiteDB())
+config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.publication = True
+config.Data.outputPrimaryDataset = 'GluGlu_HToMuMu_M125_13TeV_powheg_pythia8'
+config.Data.outputDatasetTag ='UL2017-MINIAODSIM'
+
+config.Site.storageSite = 'T2_CH_CERN'
+#config.Site.storageSite = 'T3_US_MIT'
+
