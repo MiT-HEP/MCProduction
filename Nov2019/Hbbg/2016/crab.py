371a520881fff03090b112fda893c39e044863b7
Andrea Carlo Marini <andrea.marini@cern.ch>
2020-08-12 13:37:56 +0200

    H->Zg->bbg

---
+++ Nov2019/Hbbg/2016/crab.py
@@ -0,0 +1,28 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = 'amarini_FullSim_GGHMH125_powheg'
+config.General.workArea = 'crab_privateMCProduction'
+config.General.transferOutputs = True
+config.General.transferLogs = False
+
+config.JobType.pluginName = 'PrivateMC'
+#config.JobType.disableAutomaticOutputCollection = True
+config.JobType.maxMemoryMB = 2500
+config.JobType.psetName = 'fake.py' ## fake the last step -> step4 + empty source
+config.JobType.inputFiles = ['scriptExe.sh', 'step1.py','step2.py','step3.py','step4.py','pu.py']
+config.JobType.scriptExe='scriptExe.sh'
+config.JobType.numCores=4
+
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = 500
+config.Data.totalUnits = 500000
+config.Data.outLFNDirBase = '/store/group/phys_higgs/cmshmm/amarini/'
+#config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.publication = True
+config.Data.outputPrimaryDataset = 'GluGlu_HiggsZG_Zbb_M125_13TeV_powheg_pythia8'
+config.Data.outputDatasetTag ='FullSim_94X-MINIAODSIM'
+
+config.Site.storageSite = 'T2_CH_CERN'
+#config.Site.storageSite = 'T3_US_MIT'
+
