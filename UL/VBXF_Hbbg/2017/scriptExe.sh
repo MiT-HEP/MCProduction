0ee0841868f5938abd91e0a27c874a4ae3c62328
Andrea Carlo Marini <andrea.marini@cern.ch>
2020-08-26 13:37:01 +0200

    adding UL setups

---
+++ UL/VBXF_Hbbg/2017/scriptExe.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+echo "================= CMSRUN starting jobNum=$1 ====================" | tee -a job.log
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export SCRAM_ARCH=slc7_amd64_gcc700
+
+BASE=$PWD
+
+
+MYCMSSW=CMSSW_10_6_4
+    echo "================= CMSRUN setting up $MYCMSSW ===================="| tee -a job.log
+    if [ -r $MYCMSSW/src ] ; then 
+     echo release $MYCMSSW already exists
+    else
+     scram p CMSSW $MYCMSSW
+    fi
+    cd $MYCMSSW/src
+    eval `scram runtime -sh`
+    scram b 
+    cd $BASE
+
+
+echo "================= CMSRUN starting Step 1 ====================" | tee -a job.log
+cmsRun -j step1.log step1_cfg.py jobNum=$1
+
+MYCMSSW=CMSSW_10_6_2
+    echo "================= CMSRUN setting up $MYCMSSW ===================="| tee -a job.log
+    if [ -r $MYCMSSW/src ] ; then 
+     echo release $MYCMSSW already exists
+    else
+     scram p CMSSW $MYCMSSW
+    fi
+    cd $MYCMSSW/src
+    eval `scram runtime -sh`
+    scram b 
+    cd $BASE
+
+echo "================= CMSRUN starting Step 2 ====================" | tee -a job.log
+cmsRun -j step2.log step2_cfg.py
+
+CLEAN=step1.root
+    echo "--> cleaning up $CLEAN"
+    rm -v $CLEAN
+
+echo "================= CMSRUN starting Step 3 ====================" | tee -a job.log
+cmsRun -j step3.log step3_cfg.py
+
+CLEAN=step2.root
+    echo "--> cleaning up $CLEAN"
+    rm -v $CLEAN
+
+MYCMSSW=CMSSW_9_4_14_UL_patch1
+    echo "================= CMSRUN setting up $MYCMSSW ===================="| tee -a job.log
+    if [ -r $MYCMSSW/src ] ; then 
+     echo release $MYCMSSW already exists
+    else
+     scram p CMSSW $MYCMSSW
+    fi
+    cd $MYCMSSW/src
+    eval `scram runtime -sh`
+    scram b 
+    cd $BASE
+
+echo "================= CMSRUN starting Step 4 ====================" | tee -a job.log
+cmsRun -j step4.log step4_cfg.py
+
+CLEAN=step3.root
+    echo "--> cleaning up $CLEAN"
+    rm -v $CLEAN
+
+MYCMSSW=CMSSW_10_6_2
+    echo "================= CMSRUN setting up $MYCMSSW ===================="| tee -a job.log
+    if [ -r $MYCMSSW/src ] ; then 
+     echo release $MYCMSSW already exists
+    else
+     scram p CMSSW $MYCMSSW
+    fi
+    cd $MYCMSSW/src
+    eval `scram runtime -sh`
+    scram b 
+    cd $BASE
+
+echo "================= CMSRUN starting Step 5 ====================" | tee -a job.log
+cmsRun -j step5.log step5_cfg.py
+
+CLEAN=step4.root
+    echo "--> cleaning up $CLEAN"
+    rm -v $CLEAN
+
+echo "================= CMSRUN starting Step 6 ====================" | tee -a job.log
+cmsRun -e -j FrameworkJobReport.xml step6_cfg.py
+
+
+echo "================= CMSRUN finished ====================" | tee -a job.log
