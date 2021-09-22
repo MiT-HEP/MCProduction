1c0f3d9fc77f26d4b1007d85649a3eed10074785
Andrea Carlo Marini <andrea.marini@cern.ch>
2021-09-22 16:55:13 +0200

    adding more info to request name

--- UL_NanoAODv9/2016_HIPM/crab.py
+++ UL_NanoAODv9/2016_HIPM/crab.py
@@ -1,7 +1,7 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'amarini_UL2016_GGHMH125_powheg'
+config.General.requestName = 'amarini_UL2016_HIPM_GGHMH125_powheg'
 config.General.workArea = 'crab_privateMCProduction'
 config.General.transferOutputs = True
 config.General.transferLogs = False
@@ -31,151 +31,151 @@ config.Site.storageSite = 'T2_CH_CERN'
 do='xxx'
 if do=='xxx': raise ValueError('Set do')
 if do=='hbbg' :
-    config.General.requestName = 'amarini-UL2016_GGHMH125_powheg'
+    config.General.requestName = 'amarini-UL2016_HIPM_GGHMH125_powheg'
     config.Data.outputPrimaryDataset = 'GluGlu_HiggsZG_Zbb_M125_13TeV_powheg_pythia8'
     config.JobType.scriptArgs=['chain=hbbg']
 if do=='vbf-hbbg':
-    config.General.requestName = 'amarini-UL2016_VBFMH125_powheg'
+    config.General.requestName = 'amarini-UL2016_HIPM_VBFMH125_powheg'
     config.Data.outputPrimaryDataset = 'VBF_HiggsZG_Zbb_M125_13TeV_powheg_pythia8'
     config.JobType.scriptArgs=['chain=vbf-hbbg']
 if do=='hwz1500' :
-    config.General.requestName = 'amarini_UL2016_SinglyChargedHiggsGMmodel_HWZ_Znn_M1500_madgraph'
+    config.General.requestName = 'amarini_UL2016_HIPM_SinglyChargedHiggsGMmodel_HWZ_Znn_M1500_madgraph'
     config.Data.outputPrimaryDataset = 'SinglyChargedHiggsGMmodel_HWZ_Znn_M1500_13TeV-madgraph'
     config.JobType.scriptArgs=['chain=hplus_hwz_1500']
 if do=='hwz1000' :
-    config.General.requestName = 'amarini_UL2016_SinglyChargedHiggsGMmodel_HWZ_Znn_M1000_madgraph'
+    config.General.requestName = 'amarini_UL2016_HIPM_SinglyChargedHiggsGMmodel_HWZ_Znn_M1000_madgraph'
     config.Data.outputPrimaryDataset = 'SinglyChargedHiggsGMmodel_HWZ_Znn_M1000_13TeV-madgraph'
     config.JobType.scriptArgs=['chain=hplus_hwz_1000']
 if do=='hwz2000' :
-    config.General.requestName = 'amarini_UL2016_SinglyChargedHiggsGMmodel_HWZ_Znn_M2000_madgraph'
+    config.General.requestName = 'amarini_UL2016_HIPM_SinglyChargedHiggsGMmodel_HWZ_Znn_M2000_madgraph'
     config.Data.outputPrimaryDataset = 'SinglyChargedHiggsGMmodel_HWZ_Znn_M2000_13TeV-madgraph'
     config.JobType.scriptArgs=['chain=hplus_hwz_2000']
 if do=='hwz3000' :
-    config.General.requestName = 'amarini_UL2016_SinglyChargedHiggsGMmodel_HWZ_Znn_M3000_madgraph'
+    config.General.requestName = 'amarini_UL2016_HIPM_SinglyChargedHiggsGMmodel_HWZ_Znn_M3000_madgraph'
     config.Data.outputPrimaryDataset = 'SinglyChargedHiggsGMmodel_HWZ_Znn_M3000_13TeV-madgraph'
     config.JobType.scriptArgs=['chain=hplus_hwz_3000']
 if do=='hww1500' :
-    config.General.requestName = 'amarini_UL2016_DoublyChargedHiggsGMmodel_HWW_M1500_13TeV-madgraph'
+    config.General.requestName = 'amarini_UL2016_HIPM_DoublyChargedHiggsGMmodel_HWW_M1500_13TeV-madgraph'
     config.Data.outputPrimaryDataset = 'DoublyChargedHiggsGMmodel_HWW_M1500_13TeV-madgraph'
     config.JobType.scriptArgs=['chain=hplusplus_hww_1500']
 if do=='hww1000' :
-    config.General.requestName = 'amarini_UL2016_DoublyChargedHiggsGMmodel_HWW_M1000_13TeV-madgraph'
+    config.General.requestName = 'amarini_UL2016_HIPM_DoublyChargedHiggsGMmodel_HWW_M1000_13TeV-madgraph'
     config.Data.outputPrimaryDataset = 'DoublyChargedHiggsGMmodel_HWW_M1000_13TeV-madgraph'
     config.JobType.scriptArgs=['chain=hplusplus_hww_1000']
 if do=='hww2000' :
-    config.General.requestName = 'amarini_UL2016_DoublyChargedHiggsGMmodel_HWW_M2000_13TeV-madgraph'
+    config.General.requestName = 'amarini_UL2016_HIPM_DoublyChargedHiggsGMmodel_HWW_M2000_13TeV-madgraph'
     config.Data.outputPrimaryDataset = 'DoublyChargedHiggsGMmodel_HWW_M2000_13TeV-madgraph'
     config.JobType.scriptArgs=['chain=hplusplus_hww_2000']
 if do=='hww3000' :
-    config.General.requestName = 'amarini_UL2016_DoublyChargedHiggsGMmodel_HWW_M3000_13TeV-madgraph'
+    config.General.requestName = 'amarini_UL2016_HIPM_DoublyChargedHiggsGMmodel_HWW_M3000_13TeV-madgraph'
     config.Data.outputPrimaryDataset = 'DoublyChargedHiggsGMmodel_HWW_M3000_13TeV-madgraph'
     config.JobType.scriptArgs=['chain=hplusplus_hww_3000']
 if do=='hwz_zbb_1500' :
-    config.General.requestName = 'amarini_UL2016_SinglyChargedHiggsGMmodel_HWZ_Zbb_M1500_madgraph'
+    config.General.requestName = 'amarini_UL2016_HIPM_SinglyChargedHiggsGMmodel_HWZ_Zbb_M1500_madgraph'
     config.Data.outputPrimaryDataset = 'SinglyChargedHiggsGMmodel_HWZ_Zbb_M1500_13TeV-madgraph'
     config.JobType.scriptArgs=['chain=hplus_hwz_zbb_1500']
 if do=='hwz_zbb_1000' :
-    config.General.requestName = 'amarini_UL2016_SinglyChargedHiggsGMmodel_HWZ_Zbb_M1000_madgraph'
+    config.General.requestName = 'amarini_UL2016_HIPM_SinglyChargedHiggsGMmodel_HWZ_Zbb_M1000_madgraph'
     config.Data.outputPrimaryDataset = 'SinglyChargedHiggsGMmodel_HWZ_Zbb_M1000_13TeV-madgraph'
     config.JobType.scriptArgs=['chain=hplus_hwz_zbb_1000']
 if do=='hwz_zbb_2000' :
-    config.General.requestName = 'amarini_UL2016_SinglyChargedHiggsGMmodel_HWZ_Zbb_M2000_madgraph'
+    config.General.requestName = 'amarini_UL2016_HIPM_SinglyChargedHiggsGMmodel_HWZ_Zbb_M2000_madgraph'
     config.Data.outputPrimaryDataset = 'SinglyChargedHiggsGMmodel_HWZ_Zbb_M2000_13TeV-madgraph'
     config.JobType.scriptArgs=['chain=hplus_hwz_zbb_2000']
 if do=='hwz_zbb_3000' :
-    config.General.requestName = 'amarini_UL2016_SinglyChargedHiggsGMmodel_HWZ_Zbb_M3000_madgraph'
+    config.General.requestName = 'amarini_UL2016_HIPM_SinglyChargedHiggsGMmodel_HWZ_Zbb_M3000_madgraph'
     config.Data.outputPrimaryDataset = 'SinglyChargedHiggsGMmodel_HWZ_Zbb_M3000_13TeV-madgraph'
     config.JobType.scriptArgs=['chain=hplus_hwz_zbb_3000']
 if do=='WWjj_ss_pol_hadronic_ll' :
-    config.General.requestName = 'amarini_UL2016_WWjj_SS_ll_hadronic'
+    config.General.requestName = 'amarini_UL2016_HIPM_WWjj_SS_ll_hadronic'
     config.Data.outputPrimaryDataset = 'WWjj_SS_ll_hadronic'
     config.JobType.scriptArgs=['chain=WWjj_ss_pol_hadronic_ll']
 if do=='WWjj_ss_pol_hadronic_lt' :
-    config.General.requestName = 'amarini_UL2016_WWjj_SS_lt_hadronic'
+    config.General.requestName = 'amarini_UL2016_HIPM_WWjj_SS_lt_hadronic'
     config.Data.outputPrimaryDataset = 'WWjj_SS_lt_hadronic'
     config.JobType.scriptArgs=['chain=WWjj_ss_pol_hadronic_lt']
 if do=='WWjj_ss_pol_hadronic_tt' :
-    config.General.requestName = 'amarini_UL2016_WWjj_SS_tt_hadronic'
+    config.General.requestName = 'amarini_UL2016_HIPM_WWjj_SS_tt_hadronic'
     config.Data.outputPrimaryDataset = 'WWjj_SS_tt_hadronic'
     config.JobType.scriptArgs=['chain=WWjj_ss_pol_hadronic_tt']
 if do=='hww1000_semilep' :
-    config.General.requestName = 'amarini_UL2016_DoublyChargedHiggsGMmodel_HWW_semilep_M1000_13TeV-madgraph'
+    config.General.requestName = 'amarini_UL2016_HIPM_DoublyChargedHiggsGMmodel_HWW_semilep_M1000_13TeV-madgraph'
     config.Data.outputPrimaryDataset = 'DoublyChargedHiggsGMmodel_HWW_semilep_M1000_13TeV-madgraph'
     config.JobType.scriptArgs=['chain=hplusplus_hww_semilep_1000']
 if do=='hww2000_semilep' :
-    config.General.requestName = 'amarini_UL2016_DoublyChargedHiggsGMmodel_HWW_semilep_M2000_13TeV-madgraph'
+    config.General.requestName = 'amarini_UL2016_HIPM_DoublyChargedHiggsGMmodel_HWW_semilep_M2000_13TeV-madgraph'
     config.Data.outputPrimaryDataset = 'DoublyChargedHiggsGMmodel_HWW_semilep_M2000_13TeV-madgraph'
     config.JobType.scriptArgs=['chain=hplusplus_hww_semilep_2000']
 
 if do=='vbs_wpjj_wmjj_ewk':
-    config.General.requestName = 'amarini_UL2016_WPJJWMJJjj_EWK_LO_TuneCP5_13TeV-madgraph-pythia8'
+    config.General.requestName = 'amarini_UL2016_HIPM_WPJJWMJJjj_EWK_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.Data.outputPrimaryDataset = 'WPJJWMJJjj_EWK_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.JobType.scriptArgs=['chain='+do]
 if do=='vbs_wpmjj_wpmjj_ewk':
-    config.General.requestName = 'amarini_UL2016_WPMJJWPMJJjj_EWK_LO_TuneCP5_13TeV-madgraph-pythia8'
+    config.General.requestName = 'amarini_UL2016_HIPM_WPMJJWPMJJjj_EWK_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.Data.outputPrimaryDataset = 'WPMJJWPMJJjj_EWK_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.JobType.scriptArgs=['chain='+do]
 if do=='vbs_zbb_wpmjj_ewk':
-    config.General.requestName = 'amarini_UL2016_ZBBWPMJJjj_EWK_LO_TuneCP5_13TeV-madgraph-pythia8'
+    config.General.requestName = 'amarini_UL2016_HIPM_ZBBWPMJJjj_EWK_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.Data.outputPrimaryDataset = 'ZBBWPMJJjj_EWK_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.JobType.scriptArgs=['chain='+do]
 if do=='vbs_zjj_zjj_ewk':
-    config.General.requestName = 'amarini_UL2016_ZJJZJJjj_EWK_LO_TuneCP5_13TeV-madgraph-pythia8'
+    config.General.requestName = 'amarini_UL2016_HIPM_ZJJZJJjj_EWK_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.Data.outputPrimaryDataset = 'ZJJZJJjj_EWK_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.JobType.scriptArgs=['chain='+do]
 if do=='vbs_znn_wpmjj_ewk':
-    config.General.requestName = 'amarini_UL2016_ZNuNuWPMJJjj_EWK_LO_TuneCP5_13TeV-madgraph-pythia8'
+    config.General.requestName = 'amarini_UL2016_HIPM_ZNuNuWPMJJjj_EWK_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.Data.outputPrimaryDataset = 'ZNuNuWPMJJjj_EWK_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.JobType.scriptArgs=['chain='+do]
 
 if do=='vbs_wpjj_wmjj_qcd':
-    config.General.requestName = 'amarini_UL2016_WPJJWMJJjj_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
+    config.General.requestName = 'amarini_UL2016_HIPM_WPJJWMJJjj_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.Data.outputPrimaryDataset = 'WPJJWMJJjj_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.JobType.scriptArgs=['chain='+do]
 if do=='vbs_wpmjj_wpmjj_qcd':
-    config.General.requestName = 'amarini_UL2016_WPMJJWPMJJjj_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
+    config.General.requestName = 'amarini_UL2016_HIPM_WPMJJWPMJJjj_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.Data.outputPrimaryDataset = 'WPMJJWPMJJjj_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.JobType.scriptArgs=['chain='+do]
 if do=='vbs_zbb_wpmjj_qcd':
-    config.General.requestName = 'amarini_UL2016_ZBBWPMJJjj_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
+    config.General.requestName = 'amarini_UL2016_HIPM_ZBBWPMJJjj_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.Data.outputPrimaryDataset = 'ZBBWPMJJjj_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.JobType.scriptArgs=['chain='+do]
 if do=='vbs_zjj_zjj_qcd':
-    config.General.requestName = 'amarini_UL2016_ZJJZJJjj_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
+    config.General.requestName = 'amarini_UL2016_HIPM_ZJJZJJjj_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.Data.outputPrimaryDataset = 'ZJJZJJjj_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.JobType.scriptArgs=['chain='+do]
 if do=='vbs_znn_wpmjj_qcd':
-    config.General.requestName = 'amarini_UL2016_ZNuNuWPMJJjj_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
+    config.General.requestName = 'amarini_UL2016_HIPM_ZNuNuWPMJJjj_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.Data.outputPrimaryDataset = 'ZNuNuWPMJJjj_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.JobType.scriptArgs=['chain='+do]
 
 if do=='vbs_wpjj_wmjj_ewk_qcd':
-    config.General.requestName = 'amarini_UL2016_WPJJWMJJjj_EWK_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
+    config.General.requestName = 'amarini_UL2016_HIPM_WPJJWMJJjj_EWK_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.Data.outputPrimaryDataset = 'WPJJWMJJjj_EWK_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.JobType.scriptArgs=['chain='+do]
 if do=='vbs_wpmjj_wpmjj_ewk_qcd':
-    config.General.requestName = 'amarini_UL2016_WPMJJWPMJJjj_EWK_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
+    config.General.requestName = 'amarini_UL2016_HIPM_WPMJJWPMJJjj_EWK_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.Data.outputPrimaryDataset = 'WPMJJWPMJJjj_EWK_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.JobType.scriptArgs=['chain='+do]
 if do=='vbs_zbb_wpmjj_ewk_qcd':
-    config.General.requestName = 'amarini_UL2016_ZBBWPMJJjj_EWK_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
+    config.General.requestName = 'amarini_UL2016_HIPM_ZBBWPMJJjj_EWK_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.Data.outputPrimaryDataset = 'ZBBWPMJJjj_EWK_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.JobType.scriptArgs=['chain='+do]
 if do=='vbs_zjj_zjj_ewk_qcd':
-    config.General.requestName = 'amarini_UL2016_ZJJZJJjj_EWK_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
+    config.General.requestName = 'amarini_UL2016_HIPM_ZJJZJJjj_EWK_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.Data.outputPrimaryDataset = 'ZJJZJJjj_EWK_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.JobType.scriptArgs=['chain='+do]
 if do=='vbs_znn_wpmjj_ewk_qcd':
-    config.General.requestName = 'amarini_UL2016_ZNuNuWPMJJjj_EWK_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
+    config.General.requestName = 'amarini_UL2016_HIPM_ZNuNuWPMJJjj_EWK_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.Data.outputPrimaryDataset = 'ZNuNuWPMJJjj_EWK_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.JobType.scriptArgs=['chain='+do]
 
 if do=='vbs_hbb_wpmjj_ewk':
-    config.General.requestName = 'amarini_UL2016_HBBWPMJJjj_EWK_LO_TuneCP5_13TeV-madgraph-pythia8'
+    config.General.requestName = 'amarini_UL2016_HIPM_HBBWPMJJjj_EWK_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.Data.outputPrimaryDataset = 'HBBWPMJJjj_EWK_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.JobType.scriptArgs=['chain='+do]
 if do=='vbs_hbb_wpmjj_qcd':
-    config.General.requestName = 'amarini_UL2016_HBBWPMJJjj_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
+    config.General.requestName = 'amarini_UL2016_HIPM_HBBWPMJJjj_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.Data.outputPrimaryDataset = 'HBBWPMJJjj_QCD_LO_TuneCP5_13TeV-madgraph-pythia8'
     config.JobType.scriptArgs=['chain='+do]
 
